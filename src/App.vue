77a4b665c70a424759f635c2b7ea9b4cde3b1c59
Ermand Durro <ermand.duro@gmail.com>
2018-10-28 18:24:17 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="container">
+    {{activeTab$}}
+    <b-tabs v-model="activeTab">
+      <b-tab-item v-for="person of people$" :key="person.id" :label="person.name"></b-tab-item>
+    </b-tabs>
+    <h1 class="title">{{ name$ }}</h1>
+    <img v-stream:error="imageError$" :src="image$" alt="">
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Observable } from 'rxjs';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      activeTab: 0
+    };
+  },
+  domStreams: ['click$', 'imageError$'],
+  subscriptions() {
+    const cache = {};
+    const cachePerson = cache => url => {
+      return cache[url] ? cache[url] : (cache[url] = createLoader(url));
+    };
+    const createLoader = url =>
+      Observable.from(this.$http.get(url)).pluck('data');
+
+    const people$ = createLoader(
+      'https://starwars.egghead.training/people'
+    ).map(people => people.slice(0, 7));
+
+    const activeTab$ = this.$watchAsObservable('activeTab', {
+      immediate: true
+    }).pluck('newValue');
+
+    const luke$ = activeTab$
+      .combineLatest(people$, (tabId, people) => people[tabId].id)
+      .map(id => `https://starwars.egghead.training/people/${id}`)
+      .switchMap(cachePerson(cache))
+      .catch(err => {
+        console.log('error:', err);
+        createLoader('https://starwars.egghead.training/people/2');
+      })
+      .share();
+
+    const disabled$ = Observable.merge(
+      this.click$.mapTo(true),
+      luke$.mapTo(false)
+    ).startWith(false);
+
+    const buttonText$ = disabled$.map(bool => (bool ? 'Loading..' : 'Load'));
+
+    const name$ = luke$.pluck('name');
+    const loadImage$ = luke$
+      .pluck('image')
+      .map(image => `https://starwars.egghead.training/${image}`);
+
+    const failImage$ = this.imageError$.mapTo(
+      `http://via.placeholder.com/400x400`
+    );
+    const image$ = Observable.merge(loadImage$, failImage$);
+
+    return {
+      name$,
+      image$,
+      disabled$,
+      buttonText$,
+      activeTab$,
+      people$
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  margin-top: 2em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
+import 'buefy/lib/buefy.css'
 import Vue from 'vue'
 import App from './App.vue'
+
+import {
+  Observable,
+  Subscription,
+  Subject
+} from 'rxjs'
+import VueRx from 'vue-rx'
+
+import Buefy from 'buefy'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueRx, {
+  Observable,
+  Subscription,
+  Subject
+})
+Vue.use(Buefy)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
